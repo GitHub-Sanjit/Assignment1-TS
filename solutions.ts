c7f0c321caa9cde8511bb85899f495771630bafe
sanjit <sanjit.est14@gmail.com>
2026-05-07 10:11:23 +0600

    Question 2

--- solutions.ts
+++ solutions.ts
@@ -5,4 +5,16 @@ const filterEvenNumbers = (numArray: number[]): number[] => {
 
 // console.log(filterEvenNumbers([1, 2, 3, 4, 5, 6]));
 
+//*  2
+
+const reverseString = (str: string): string => {
+  const lastChar = str.length - 1;
+  let reverseStr = "";
+  for (let i = lastChar; i >= 0; i--) {
+    reverseStr = reverseStr + str[i];
+  }
+  return reverseStr;
+};
+
+// console.log(reverseString("typescript"))
 
